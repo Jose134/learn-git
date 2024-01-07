f4e076af8488916f0e5ce062554dc9fd79694014
Jose134 <jmjuradobujalance@gmail.com>
2024-01-07 01:51:10 +0100

    Redirect to home if route isn't found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,6 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'article/:name', component: ArticleComponent },
     { path: 'about', component: AboutComponent },
+    
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
