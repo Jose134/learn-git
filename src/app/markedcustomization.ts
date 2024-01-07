babe894c5f832690d63b3911f396b7f8c7929855
Jose134 <jmjuradobujalance@gmail.com>
2024-01-07 23:41:31 +0100

    Improved solution codeblocks

--- src/app/markedcustomization.ts
+++ src/app/markedcustomization.ts
@@ -5,14 +5,16 @@ export function markedOptionsCustom(): MarkedOptions {
     const renderer = new MarkedRenderer();
 
     renderer.code = (text: string) => {
-        let html = '<pre>';
+        let html = '<div class="codeblock">';
         let addCss = '';
         if (text.startsWith('SOLUTION')) {
-            //html += '<div class="hover-instructions">Hover to show the solution</div>';
+            html += '<div class="hover-instructions">Hover to show the solution</div>';
+            html += '<pre>';
             addCss = ' class="code-solution"';
+            text = text.replace('SOLUTION\n', '');
         }
         
-        html += '<code' + addCss + '>' + text + '</code></pre>';
+        html += '<code' + addCss + '>' + text + '</code></pre></div>';
         return html;
     };
 
